e9589da41673e9d7544a05043c8ec242a342dbc8
as.stafeev <stayf777@gmail.com>
2021-11-27 11:00:02 +0700

    конфиги

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -14,16 +14,16 @@ import android.os.Build;
 
 public class BuildVars {
 
-    public static boolean DEBUG_VERSION = false;
-    public static boolean LOGS_ENABLED = false;
-    public static boolean DEBUG_PRIVATE_VERSION = false;
+    public static boolean DEBUG_VERSION = true;
+    public static boolean LOGS_ENABLED = true;
+    public static boolean DEBUG_PRIVATE_VERSION = true;
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = true;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static int BUILD_VERSION = 2481;
     public static String BUILD_VERSION_STRING = "8.3.0";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static int APP_ID = 15620389;
+    public static String APP_HASH = "7f0168e0c6b363cb1464c192234756fc";
     public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
 
@@ -37,7 +37,7 @@ public class BuildVars {
     private static Boolean standaloneApp;
     public static boolean isStandaloneApp() {
         if (standaloneApp == null) {
-            standaloneApp = ApplicationLoader.applicationContext != null && "org.telegram.messenger.web".equals(ApplicationLoader.applicationContext.getPackageName());
+            standaloneApp = ApplicationLoader.applicationContext != null && "org.telegram.messengerstayf.web".equals(ApplicationLoader.applicationContext.getPackageName());
         }
         return standaloneApp;
     }
@@ -45,7 +45,7 @@ public class BuildVars {
     private static Boolean betaApp;
     public static boolean isBetaApp() {
         if (betaApp == null) {
-            betaApp = ApplicationLoader.applicationContext != null && "org.telegram.messenger.beta".equals(ApplicationLoader.applicationContext.getPackageName());
+            betaApp = ApplicationLoader.applicationContext != null && "org.telegram.messengerstayf.beta".equals(ApplicationLoader.applicationContext.getPackageName());
         }
         return betaApp;
     }
